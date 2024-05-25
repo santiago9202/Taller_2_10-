36471863ee095e48a1c8ba052b0c81c44a10c5e8
SANTIAGO OSORIO OCHOA <santiagoosorio1120920@correo.itm.edu.co>
2024-05-24 20:30:16 -0500

    se establece la posición inicial del jugador y se inicia el bucle principal del juego.

--- Program.cs
+++ Program.cs
@@ -5,10 +5,8 @@
         static void Main(string[] args)
         {
             int[,] tablero = new int[5, 5]; // Crear una matriz 5x5
-            Random rand = new Random();
             int posX = 0, posY = 0; // Posición inicial en [0,0]
             int puntaje = 0;
-
 
         }
     }
