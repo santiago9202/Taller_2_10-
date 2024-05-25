afe00833a7db288d840bb5fbc131c9403a8c23b0
SANTIAGO OSORIO OCHOA <santiagoosorio1120920@correo.itm.edu.co>
2024-05-24 20:21:19 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Taller_2_10_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tablero = new int[5, 5]; // Crear una matriz 5x5
+            Random rand = new Random();
+            int posX = 0, posY = 0; // Posición inicial en [0,0]
+            int puntaje = 0;
+
+
+        }
+    }
+}
